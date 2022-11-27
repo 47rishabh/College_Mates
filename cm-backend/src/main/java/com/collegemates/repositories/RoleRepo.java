8005d9394c0050616e7a37b9f9a8a672e4d76086
47rishabh <rishabhbelsare47@gmail.com>
2022-11-27 19:23:37 +0530

    frontend

--- cm-backend/src/main/java/com/collegemates/repositories/RoleRepo.java
+++
@@ -1,7 +0,0 @@
-package com.collegemates.repositories;
-
-import com.collegemates.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Integer> {
-}
